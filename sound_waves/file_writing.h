ca293f454be2c1adf769d1c4cc74367be65d5ce2
BbqCoder <korba.adam@gmail.com>
2020-03-25 17:23:18 +0100

    Revert "Revert "Objectize file_writing""
    This reverts commit b0733ab3c643bb64794c8e1a5367e3bcbda6d98c.

--- sound_waves/file_writing.h
+++ sound_waves/file_writing.h
@@ -1,20 +1,31 @@
 #ifndef file_writing_h
 #define file_writing_h
 
+#include "waveform.h"
 #include <string>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
 
-ofstream create_wav_file(string filename, int num_hannels, int sample_rate, int bit_depth); //function to create file and add headers
-
-
-void complete_wav_file(ofstream & f, size_t data_chunk_pos); //function that completes missing headers of a file
-
-
-template <typename Word>
-ostream& write_word(ostream& outs, Word value, unsigned size = sizeof(Word)); //writes choosen word to binary file
-
+class WavFileProcessor
+{
+    ofstream file_stream;
+    string filename;
+    int num_channels;
+    int sample_rate;
+    int bit_depth;
+    
+    size_t data_chunk_pos;
+    
+    template <typename Word>
+    void write_word(Word value, unsigned size = sizeof(Word)); //writes choosen word to binary file
+    
+    public:
+        
+        WavFileProcessor(string Filename, int Num_channels, int Sample_rate, int Bit_depth);
+        ~WavFileProcessor();
+    
+};
 
 #endif
