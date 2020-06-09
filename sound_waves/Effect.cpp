c086db19088033824b0ab56e187acbd7c325089e
BbqCoder <korba.adam@gmail.com>
2020-06-09 18:44:55 +0200

    Delete redundant files Fileprocessor and WavFileprocessor

---
+++ sound_waves/Effect.cpp
@@ -0,0 +1,9 @@
+//
+//  Effect.cpp
+//  sound_waves
+//
+//  Created by Adam Korba on 09/06/2020.
+//  Copyright © 2020 Adam Korba. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ sound_waves/Effect.h
@@ -0,0 +1,13 @@
+//
+//  Effect.h
+//  sound_waves
+//
+//  Created by Adam Korba on 09/06/2020.
+//  Copyright © 2020 Adam Korba. All rights reserved.
+//
+
+#ifndef Effect_h
+#define Effect_h
+
+
+#endif /* Effect_h */
